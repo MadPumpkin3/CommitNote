b7aa01899bbf0499a66f3d3fa6bdba2b2038288e
MadPumpkin3 <https://github.com/MadPumpkin3>
2026-09-17 02:30:37 +0900

    게시글 목록 조회 기능 및 테스트 추가

--- src/main/java/me/shinsunyung/commitnote/controller/BlogApiController.java
+++ src/main/java/me/shinsunyung/commitnote/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.shinsunyung.commitnote.controller;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyung.commitnote.dto.AddArticleRequest;
 import me.shinsunyung.commitnote.domain.Article;
+import me.shinsunyung.commitnote.dto.ArticleResponse;
 import me.shinsunyung.commitnote.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -28,4 +32,14 @@ public class BlogApiController {
     }
 
     // 글 목록 조회 메서드
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
 }
